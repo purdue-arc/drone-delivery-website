4e6f432b50199d1c37c402cd8b9146dd0ab21086
funblaster22 <53224922+funblaster22@users.noreply.github.com>
2023-10-17 09:46:43 -0400

    Realtime works!

--- src/routes/gql.tsx
+++ src/routes/gql.tsx
@@ -1,22 +1,29 @@
-import {ApolloClient, gql, HttpLink, InMemoryCache, split} from '@apollo/client';
+import {gql, HttpLink, InMemoryCache, split} from '@apollo/client/core';
 import {GraphQLWsLink} from "@apollo/client/link/subscriptions";
 import {createClient} from "graphql-ws";
 import {getMainDefinition} from '@apollo/client/utilities';
+import {ApolloClient, ApolloProvider, createSubscription} from '@merged/solid-apollo'
 
 
 // Adapted from https://hasura.io/docs/latest/subscriptions/integrations/apollo-subscriptions/
 // Docs: https://www.apollographql.com/docs/react/data/subscriptions/
-// Apollo doesn't natively support solid, and it seems like the unmaintained adapter has an issue with solid start
-// https://github.com/merged-js/solid-apollo/issues/19
+// Solid adapter: https://github.com/merged-js/solid-apollo
 
 const HASURA_URL = "dd-hasura.purduearc.com/v1/graphql";
+const headers = {
+  "x-hasura-admin-secret": import.meta.env["VITE_HASURA_ADMIN_SECRET"],
+};
 
 const httpLink = new HttpLink({
   uri: 'https://' + HASURA_URL,
+  headers,
 });
 
 const wsLink = new GraphQLWsLink(createClient({
   url: 'wss://' + HASURA_URL,
+  connectionParams: {
+    headers,
+  }
 }));
 
 const splitLink = split(
@@ -37,22 +44,27 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
-const COMMENTS_SUBSCRIPTION = gql`
-  subscription OnCommentAdded($postID: ID!) {
-    commentAdded(postID: $postID) {
+const SUBSCRIPTION = gql`
+  subscription GetCountriesStreamingSubscription {
+    countries {
       id
-      content
+      name
     }
   }
 `;
 
-function LatestComment({ postID }) {
-  const { data, loading } = useSubscription(
-    COMMENTS_SUBSCRIPTION,
-    { variables: { postID } }
+export default function Gql() {
+  return (
+    <ApolloProvider client={client}>
+      <AppLol />
+    </ApolloProvider>
   );
-  return <h4>New comment: {!loading && data.commentAdded.content}</h4>;
 }
 
-export default function Gql() {
+function AppLol() {
+  const data = createSubscription(SUBSCRIPTION);
+
+  return (
+    <div style={{"padding-left": "12px"}}>{JSON.stringify(data(), null, 2)}</div>
+  )
 }
