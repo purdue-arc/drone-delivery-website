ed8d87b9231a5e320ff5d76d54345af248f0aded
funblaster22 <53224922+funblaster22@users.noreply.github.com>
2023-10-21 17:50:40 -0400

    fix import error
    I should've known better that `import vite` on client would fail

--- codegen.ts
+++ codegen.ts
@@ -1,12 +1,15 @@
 import type {CodegenConfig} from '@graphql-codegen/cli'
-import {GRAPHQL_ENDPOINT, GRAPHQL_HEADERS} from "./src/lib/gqlClient";
+import {GRAPHQL_ENDPOINT} from "./src/lib/gqlClient";
+import {loadEnv} from "vite";
 
 // Documentation: https://the-guild.dev/graphql/codegen/docs/config-reference/codegen-config
 
 const config: CodegenConfig = {
   schema: [{
     ["https://" + GRAPHQL_ENDPOINT]: {
-      headers: GRAPHQL_HEADERS,
+      headers: {
+        "x-hasura-admin-secret": loadEnv("production", process.cwd())["VITE_HASURA_ADMIN_SECRET"] as string,
+      },
     },
   }],
   documents: ['src/**/*.tsx'],
