5ff34e0405fd3cb3376f41c26826a11ad6b119d7
Ethan Dawes <53224922+funblaster22@users.noreply.github.com>
2023-10-22 00:41:49 +0000

    Add docs

--- codegen.ts
+++ codegen.ts
@@ -21,6 +21,7 @@ const config: CodegenConfig = {
         useTypeImports: true
       }
     },
+    // Docs: https://the-guild.dev/graphql/codegen/plugins/other/schema-ast
     './src/gql/schema.graphql': {
       plugins: ['schema-ast']
     }
