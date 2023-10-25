54b2d731792ad5462da39ace1f769f105d71b689
funblaster22 <53224922+funblaster22@users.noreply.github.com>
2023-10-24 22:52:52 -0400

    fix rendering issues

--- src/components/cards/DroneStatusCard.tsx
+++ src/components/cards/DroneStatusCard.tsx
@@ -7,6 +7,7 @@ import {
   Battery5Bar,
   Battery6Bar,
   BatteryFull,
+  BatteryUnknown,
   ExploreOutlined as CompassIcon,
   PlaceOutlined as PlaceIcon,
   SpeedOutlined as SpeedIcon
@@ -39,7 +40,8 @@ const LoadingElem = () => <span>loading...</span>;
 
 export default function DroneStatusCard() {
   const params = useParams();
-  const droneInfo = createSubscription(statusQuery);
+  const droneInfo = createSubscription(statusQuery, {variables: {droneId: params.id}});
+  // TODO: direct to error page if drone ID doesn't exist
 
   return (
     <Card variant="outlined" sx={{display: "inline-block", padding: 2}}>
@@ -50,8 +52,10 @@ export default function DroneStatusCard() {
           </Box>
           <p>
             <PlaceIcon sx={{ marginRight: '0.5em' }} />
+            {/* IDE shows errors, but trust, my type guards are foolproof */}
+            {/* TODO: is there an async way of doing this?*/}
             <Show
-              when={droneInfo()}
+              when={droneInfo()?.now}
               fallback={LoadingElem()}
             >
               ({droneInfo().now.latitude}, {droneInfo().now.longitude})
@@ -60,7 +64,7 @@ export default function DroneStatusCard() {
           <p>
             <CompassIcon sx={{ marginRight: '0.5em' }} />
             <Show
-              when={droneInfo()}
+              when={droneInfo()?.now}
               fallback={LoadingElem()}
             >
               {droneInfo().now.heading}
@@ -69,16 +73,16 @@ export default function DroneStatusCard() {
           <p>
             <SpeedIcon sx={{ marginRight: '0.5em' }} />
             <Show
-              when={droneInfo()}
+              when={droneInfo()?.now}
               fallback={LoadingElem()}
             >
               {droneInfo().now.velocity} mph
             </Show>
           </p>
           <p>
-            <Dynamic component={batteryStatus[Math.round(droneInfo().now.battery() / 100 * (batteryStatus.length - 1))]} sx={{ marginRight: '0.5em' }} />
+            <Dynamic component={droneInfo()?.now ? batteryStatus[Math.round(droneInfo().now.battery / 100 * (batteryStatus.length - 1))] : BatteryUnknown} sx={{ marginRight: '0.5em' }} />
             <Show
-              when={droneInfo()}
+              when={droneInfo()?.now}
               fallback={LoadingElem()}
             >
               {droneInfo().now.battery}%
