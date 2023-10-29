95fcfb792653a7c498e6324625dd757b348c5f39
funblaster22 <53224922+funblaster22@users.noreply.github.com>
2023-10-28 20:19:43 -0400

    Make DroneStatusCard smarter

--- src/components/cards/DroneStatusCard.tsx
+++ src/components/cards/DroneStatusCard.tsx
@@ -7,102 +7,117 @@ import {
   Battery5Bar,
   Battery6Bar,
   BatteryFull,
-  BatteryUnknown,
   ExploreOutlined as CompassIcon,
   PlaceOutlined as PlaceIcon,
   SpeedOutlined as SpeedIcon
 } from '@suid/icons-material';
-import {Dynamic} from "solid-js/web";
+import {Dynamic, Match, Switch} from "solid-js/web";
 import {Box, Button, Card, Grid, Stack, Typography} from "@suid/material";
 import {useParams} from "@solidjs/router";
 import {graphql} from "~/gql";
 import {createSubscription} from "@merged/solid-apollo";
-import {Show} from "solid-js";
+import {createEffect, Show} from "solid-js";
 
 const batteryStatus = [Battery0Bar, Battery1Bar, Battery2Bar, Battery3Bar, Battery4Bar, Battery5Bar, Battery6Bar, BatteryFull];
 
 const statusQuery = graphql(`
-  subscription DroneInfo($droneId: bigint!) {
-      now: drone_telemetry_by_pk(id: $droneId) {
-          velocity
-          latitude
-          longitude
-          heading
-          timestamp
-          battery
-          altitude
-      }
-  }
+    subscription DroneInfo($droneId: bigint!) {
+        me: drones_by_pk(drone_id: $droneId) {
+            telemetry: drone_telemetries(limit: 1, order_by: {timestamp: desc}) {
+                altitude
+                battery
+                heading
+                has_package
+                latitude
+                longitude
+                stage_of_flight
+                timestamp
+                velocity
+            }
+            flights(limit: 1, order_by: {flight_id: desc}) {
+                status
+                order {
+                    placed_at
+                    vendor {
+                        name
+                    }
+                }
+            }
+        }
+    }
 `)
+// TODO: placed_at is currently unused
 
-const LoadingElem = () => <span>loading...</span>;
+const LoadingElem = () => <span>Drone has not produced enough data</span>;
+const CONCERNING_LAG = 5000;  // in ms
 
 
 export default function DroneStatusCard() {
   const params = useParams();
   const droneInfo = createSubscription(statusQuery, {variables: {droneId: params.id}});
-  // TODO: direct to error page if drone ID doesn't exist
+  const telemetry = () => droneInfo()?.me?.telemetry[0];
+  const flight = () => droneInfo()?.me?.flights[0];
+  const timeSinceUpdate = () => new Date().getTime() - telemetry()?.timestamp ?? 0;
+  createEffect(() => console.log(droneInfo()));
 
   return (
     <Card variant="outlined" sx={{display: "inline-block", padding: 2}}>
-      <Grid container spacing={2}>
-        <Grid item>
-          <Box sx={{textAlign: "center"}}>
-            <img src="/drone.jpg" width="150px" />
-          </Box>
-          <p>
-            <PlaceIcon sx={{ marginRight: '0.5em' }} />
-            {/* IDE shows errors, but trust, my type guards are foolproof */}
-            {/* TODO: is there an async way of doing this?*/}
-            <Show
-              when={droneInfo()?.now}
-              fallback={LoadingElem()}
-            >
-              ({droneInfo().now.latitude}, {droneInfo().now.longitude})
-            </Show>
-          </p>
-          <p>
-            <CompassIcon sx={{ marginRight: '0.5em' }} />
-            <Show
-              when={droneInfo()?.now}
-              fallback={LoadingElem()}
-            >
-              {droneInfo().now.heading}
-            </Show>
-          </p>
-          <p>
-            <SpeedIcon sx={{ marginRight: '0.5em' }} />
-            <Show
-              when={droneInfo()?.now}
-              fallback={LoadingElem()}
-            >
-              {droneInfo().now.velocity} mph
-            </Show>
-          </p>
-          <p>
-            <Dynamic component={droneInfo()?.now ? batteryStatus[Math.round(droneInfo().now.battery / 100 * (batteryStatus.length - 1))] : BatteryUnknown} sx={{ marginRight: '0.5em' }} />
-            <Show
-              when={droneInfo()?.now}
-              fallback={LoadingElem()}
-            >
-              {droneInfo().now.battery}%
-            </Show>
-          </p>
-        </Grid>
-        <Grid item container direction="column" width={500}>
-          <Grid item sx={{flexGrow: 99}}>
-            <Typography sx={{ fontSize: '2em', fontWeight: 'bold', textAlign: "center" }}>Drone {params.id}</Typography>
-            <Typography>✅ Operational / ⚠️ Low battery / 🚨 Crashed</Typography>
-            <Typography>Delivering to ___/picking up from _____/parking at/idle</Typography>
-          </Grid>
+      {/* IDE shows errors, but trust, my type guards are foolproof */}
+      {/* TODO: is there an async way of doing this?*/}
+      <Show
+        when={telemetry() && flight()}
+        fallback={LoadingElem()}
+      >
+        <Grid container spacing={2}>
           <Grid item>
-            <Stack spacing={2} direction="row" justifyContent="right">
-              <Button variant="outlined">View Camera</Button>
-              <Button variant="outlined">New Flight</Button>
-            </Stack>
+            <Box sx={{textAlign: "center"}}>
+              <img src="/drone.jpg" width="150px" />
+            </Box>
+            <p>
+              <PlaceIcon sx={{ marginRight: '0.5em' }} />
+              ({telemetry()!.latitude}, {telemetry()!.longitude})
+            </p>
+            <p>
+              <CompassIcon sx={{ marginRight: '0.5em' }} />
+              {telemetry()!.heading}
+            </p>
+            <p>
+              <SpeedIcon sx={{ marginRight: '0.5em' }} />
+              {telemetry()!.velocity} mph
+            </p>
+            <p>
+              <Dynamic component={batteryStatus[Math.round(telemetry()!.battery / 100 * (batteryStatus.length - 1))]} sx={{ marginRight: '0.5em' }} />
+              {telemetry()!.battery}%
+            </p>
+          </Grid>
+          <Grid item container direction="column" width={500}>
+            <Grid item sx={{flexGrow: 99, textAlign: "center"}}>
+              <Typography sx={{ fontSize: '2em', fontWeight: 'bold' }}>Drone {params.id}</Typography>
+              <Switch fallback={<Typography>✅ Operational</Typography>}>
+                <Match when={timeSinceUpdate() > CONCERNING_LAG}>
+                  <Typography>🚨 {Math.floor(timeSinceUpdate() / 1000)} seconds since last update</Typography>
+                </Match>
+                <Match when={telemetry()!.battery < 20}>
+                  <Typography>⚠️ Low battery</Typography>
+                </Match>
+                <Match when={telemetry()!.stage_of_flight !== "idle" && flight()!.status === "failed"}>
+                  <Typography>🚨 Crashed</Typography>
+                </Match>
+              </Switch>
+              {/* TODO: intelligently report states such as: Delivering to ___/picking up from _____/parking at */}
+              <Show when={telemetry()!.stage_of_flight === "in_flight"} fallback={<Typography>{telemetry()!.stage_of_flight}</Typography>}>
+                <Typography>{telemetry()!.has_package ? "Delivering" : "Picking up"} from {flight()!.order!.vendor!.name}</Typography>
+              </Show>
+            </Grid>
+            <Grid item>
+              <Stack spacing={2} direction="row" justifyContent="right">
+                <Button variant="outlined">View Camera</Button>
+                <Button variant="outlined">New Flight</Button>
+              </Stack>
+            </Grid>
           </Grid>
         </Grid>
-      </Grid>
+      </Show>
     </Card>
   );
 };
--- src/routes/drone/[id].tsx
+++ src/routes/drone/[id].tsx
@@ -4,11 +4,13 @@ import BatteryReleaseChartCard from "~/components/cards/VoltageTimeGraphCard";
 import OrderHistoryCard from "~/components/cards/OrderHistoryCard";
 
 export default function DroneStatus() {
+  // TODO: direct to error page if drone ID doesn't exist
+
   return (
     <Grid container spacing={2} p={2} columns={2}>
       <Grid item xs={2}>
         <Box justifyContent="center" width="100%" sx={{display: "flex"}}>
-          <DroneStatusCard droneId={0} />
+          <DroneStatusCard />
         </Box>
       </Grid>
       <Grid item>
