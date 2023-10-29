2af3beb4bae625bcd94b8d4c864fdb7bc7951330
Seth Deegan <jayandseth@gmail.com>
2023-10-29 21:28:01 +0000

    fix formatting and add comment

--- src/routes/drones.tsx
+++ src/routes/drones.tsx
@@ -1,8 +1,8 @@
 
-import {Box, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography} from "@suid/material";
-import {For, createSignal} from "solid-js";
-import {graphql} from "~/gql";
-import {createSubscription} from '@merged/solid-apollo'
+import { Box, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@suid/material";
+import { For, createSignal } from "solid-js";
+import { graphql } from "~/gql";
+import { createSubscription } from '@merged/solid-apollo'
 
 
 const SUBSCRIPTION = graphql(`
@@ -22,30 +22,31 @@ export default function Drones() {
 
   return (
     <Box padding={2}>
-    <Typography variant="h3">All Drones</Typography>
-    <TableContainer sx={{ borderRadius: 2, border: 1, borderColor: 'rgba(224.4, 224.4, 224.4, 1)' }} component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-          <For each={Object.keys(data()?.drone_telemetry[0] || {})}>
-          {(columnName) => <TableCell>{columnName}</TableCell> }
-          </For>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          <For each={data()?.drone_telemetry || []}>{(row) => (
-            <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-              <For each={Object.values(row)}>
-              {(cell) => (
-                <TableCell>{cell.toString()}</TableCell>
-              )}
+      <Typography variant="h3">All Drones</Typography>
+      <TableContainer sx={{ borderRadius: 2, border: 1, borderColor: 'rgba(224.4, 224.4, 224.4, 1)' }} component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <For each={Object.keys(data()?.drone_telemetry[0] || {})}>
+                {(columnName) => <TableCell>{columnName}</TableCell>}
               </For>
             </TableRow>
-          )}
-          </For>
-        </TableBody>
-      </Table>
-    </TableContainer>
-  </Box>
+          </TableHead>
+          <TableBody>
+            <For each={data()?.drone_telemetry || []}>{(row) => (
+              <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                <For each={Object.values(row)}>
+                  {(cell) => (
+                    // toString() needed to turn boolean into string
+                    <TableCell>{cell.toString()}</TableCell>
+                  )}
+                </For>
+              </TableRow>
+            )}
+            </For>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
   );
 }
