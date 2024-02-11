84659ca87d182a3194416a05746e1a9a15fbf0da
funblaster22 <53224922+funblaster22@users.noreply.github.com>
2024-02-10 22:01:07 -0500

    switch to pnpm, properly impl monorepo

--- packages/admin-dashboard/src/lib/gqlClient.ts
+++ packages/admin-dashboard/src/lib/gqlClient.ts
@@ -5,8 +5,6 @@ import {GraphQLWsLink} from "@apollo/client/link/subscriptions";
 import {createClient} from "graphql-ws";
 import {getMainDefinition} from "@apollo/client/utilities";
 import {ApolloClient} from "@merged/solid-apollo";
-import {isServer} from "solid-js/web";
-import ws from "ws";
 import {nhost} from "~/lib/nHost";
 
 
@@ -28,10 +26,6 @@ const wsLink = new GraphQLWsLink(createClient({
   connectionParams: {
     headers,
   },
-  // It doesn't really make sense to open a websocket connection for something that just fetches once,
-  // But the alternative is dynamically rewriting gql requests (subscription -> query)
-  // See https://stackoverflow.com/questions/61743153
-  webSocketImpl: isServer ? ws : undefined,
 }));
 
 const splitLink = split(
